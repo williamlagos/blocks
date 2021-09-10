058e2b45208cbb5ed7efb9f35829f04ca9a60d72
William Oliveira de Lagos <william.lagos@icloud.com>
2021-09-10 20:25:35 +0100

    Change mock structure temporarily to a Map on id request

--- pages/api/blocks/[id].ts
+++ pages/api/blocks/[id].ts
@@ -7,36 +7,37 @@ export type BlockDetail = {
   size: number
 } 
 
+type Blocks = Map<string, BlockDetail> 
+
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Array<BlockDetail>>
+  res: NextApiResponse<Array<BlockDetail | undefined>>
 ) {
   const { query: { id } } = req
-  const blocks = {
-    "g347qho7834ahf834g": { 
-      prev_block: "9hf349g1o8703g07g8", 
-      next_block: "f9ph347g934o78go47", 
-      size: 20, 
-      block_index: 1 
-    },
-    "f9ph347g934o78go47": { 
-      prev_block: "f9ph347g934o78go47", 
-      next_block: "f3q498g7934h87o34q", 
-      size: 30, 
-      block_index: 1 
-    },
-    "f3q498g7934h87o34q": { 
-      prev_block: "f3q498g7934h87o34q", 
-      next_block: "9hf349g1o8703g07g8", 
-      size: 40, 
-      block_index: 1 
-    },
-    "9hf349g1o8703g07g8": { 
-      prev_block: "9hf349g1o8703g07g8", 
-      next_block: "g347qho7834ahf834g", 
-      size: 80, 
-      block_index: 1 
-    },
-  }  
-  res.status(200).json([blocks[id.toString()]])
+  const blocks: Blocks = new Map()
+  blocks.set("g347qho7834ahf834g", { 
+    prev_block: "9hf349g1o8703g07g8", 
+    next_block: "f9ph347g934o78go47", 
+    size: 20, 
+    block_index: 1 
+  })
+  blocks.set("f9ph347g934o78go47", { 
+    prev_block: "f9ph347g934o78go47", 
+    next_block: "f3q498g7934h87o34q", 
+    size: 30, 
+    block_index: 1 
+  })
+  blocks.set("f3q498g7934h87o34q", { 
+    prev_block: "f3q498g7934h87o34q", 
+    next_block: "9hf349g1o8703g07g8", 
+    size: 40, 
+    block_index: 1 
+  })
+  blocks.set("9hf349g1o8703g07g8", { 
+    prev_block: "9hf349g1o8703g07g8", 
+    next_block: "g347qho7834ahf834g", 
+    size: 80, 
+    block_index: 1 
+  })
+  res.status(200).json([ blocks.get(id.toString()) ])
 }
