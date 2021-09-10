ce69f361991782f17f11e2f2ed3477f8d55ff29b
William Oliveira de Lagos <william.lagos@icloud.com>
2021-09-10 11:17:56 +0100

    Added configurations and grommet for components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { grommet, Grommet } from 'grommet'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Grommet theme={grommet}>
+      <Component {...pageProps} />
+    </Grommet>
+  )
 }
 export default MyApp
